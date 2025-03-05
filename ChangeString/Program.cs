e6fd71e523088d5eab81ba28e89f389d9ff569c0
RicardoMota22 <ricardomota.artist@gmail.com>
2025-03-05 21:49:18 +0000

    Exercise 2, input string and char

---
+++ ChangeString/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ChangeString
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            //ask user for input(string)
+            Console.Write("Insert String: ");
+            string s = Console.ReadLine();
+            
+
+            //ask user fot input(Character)
+            Console.Write("Insert Character: ");
+            char Cha = Console.ReadKey().KeyChar;
+
+            s2 = s.Replace('i', 'I');
+
+            
+        }
+    }
+}
