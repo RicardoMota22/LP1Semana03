9954e0f01044a168fbc4afc4020b062210e29f6e
RicardoMota22 <ricardomota.artist@gmail.com>
2025-03-05 21:00:57 +0000

    Exercício 1, still prints 8 char argument

---
+++ RightSize/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace RightSize
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+
+            foreach(string s in args)
+            {
+                if (s.Length > 3)
+                {
+                    Console.WriteLine(s);
+                    
+                }
+                if (s.Length >= 8)
+                {
+                    Console.WriteLine("[EARLY STOP]");
+                    return;
+                }
+
+            }
+
+            
+        }
+    }
+}
