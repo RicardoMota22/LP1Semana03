e01b750a1509968ad923abe44525f0a4beb822fc
RicardoMota22 <ricardomota.artist@gmail.com>
2025-03-06 23:34:39 +0000

    Exercise 4, switch

--- HeroPerk/Program.cs
+++ HeroPerk/Program.cs
@@ -1,8 +1,12 @@
 ﻿using System;
+using System.Diagnostics.Tracing;
+using System.Threading;
+
 
 namespace HeroPerk
 {
-    enum WoWClass
+    [Flags]
+    enum Perks
     {
         WarpShift ,
         Stealth,
@@ -14,7 +18,41 @@ namespace HeroPerk
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP1!");
+            
+        //Specific letters count
+        string input = args[0];
+        char[] letterToCount = { 'w', 'a', 's', 'd' };
+        Perks p_Perks = Perks.None;
+
+        if (p_Perks = Perks.None)
+        {
+            Console.WriteLine();
+        }
+
+        if (letterToCount.Equals("w")== true)
+        {
+            p_Perks ^= Perks.WarpShift;
+        }
+        if (letterToCount.Equals("a")== true)
+        {
+            p_Perks ^= Perks.Stealth;
+        }
+        if (letterToCount.Equals("s")== true)
+        {
+            p_Perks ^= Perks.AutoHeal;
+        }
+        if (letterToCount.Equals("d")== true)
+        {
+            p_Perks ^= Perks.DoubleJump;
+        }
+        
+        
+            
+       
+
+        if ((p_Perks &Perks.WarpShift) ==Perks.WarpShift)
+        Console.WriteLine("I heal you!");
+           
         }
     }
 }
