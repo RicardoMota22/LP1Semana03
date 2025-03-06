076367fd7427d6d4a24e68fa9f3f3731b67c0d26
RicardoMota22 <ricardomota.artist@gmail.com>
2025-03-06 21:44:50 +0000

    Exercise 3 done

--- ArrayMul/Program.cs
+++ ArrayMul/Program.cs
@@ -11,33 +11,55 @@ namespace ArrayMul
         {
 
             
-        if (args.Length != 6)
-        {
-            Console.WriteLine("Uso correto: dotnet run -- arg1 arg2 arg3 arg4 arg5 arg6");
-            return;
-        }
-
-        float[,] A = new float[2, 2];
-        float[] b = new float[2];
-        float[] result = new float[2];
-
-        // Preenchendo a matriz A e o vetor b
-        A[0, 0] = float.Parse(args[0]);
-        A[0, 1] = float.Parse(args[1]);
-        A[1, 0] = float.Parse(args[2]);
-        A[1, 1] = float.Parse(args[3]);
-        b[0] = float.Parse(args[4]);
-        b[1] = float.Parse(args[5]);
-
-        // Multiplicação A * b
-        for (int i = 0; i < 2; i++)
-        {
-            result[i] = 0;
-            for (int j = 0; j < 2; j++)
+            //ask console for 6 args
+            foreach (string s in args)
             {
-                result[i] += A[i, j] * b[j];
+                if (args.Length != 6)
+                {
+                    Console.WriteLine("insert 6 floats ");
+                    return;
+                }
+                
+
             }
-        }
+
+            float[,] m2 = new float[2, 2]; //first 4 args
+            float[] v = new float[2]; //last 2 args
+            float[] result = new float[2]; // the multiplied value
+
+            //Parse first 4 args into float
+            int index = 0;
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    m2[i, j] = float.Parse(args[index], CultureInfo.InvariantCulture);
+                    index++;
+                }
+            }
+
+            //Parse last 4 args into float
+            for (int i = 0; i < 2; i++)
+            {
+                v[i] = float.Parse(args[index], CultureInfo.InvariantCulture);
+                index++;
+            }
+
+            //Multiply x2(matriz 2x2) and v(vector 1x2)
+            for (int i = 0; i < 2; i ++)
+            {
+                result[i]=0;
+                for (int j = 0; j < 2; j++)
+                {
+                    result[i] += m2[i, j] * v[j];
+                }
+            }
+            
+            foreach (var value in result)
+            {
+                Console.WriteLine("|{0,7:F2}|", value); //Formats spaces
+            }
+
 
             
 
