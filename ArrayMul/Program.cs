8853dbc1b7db52c77a25f71166655190364d1a74
RicardoMota22 <ricardomota.artist@gmail.com>
2025-03-06 16:56:04 +0000

    Some of Ex 3 e Ex 4

---
+++ ArrayMul/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ArrayMul
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+
+
+            //ask console for 6 args
+            foreach (string s in args)
+            {
+                if (s.Length == 0)
+                {
+                    Console.WriteLine("[EARLY STOP]");
+                    return;
+                }
+                if (s.Length > 0)
+                {
+                    Console.WriteLine(s);
+                }
+
+            }
+
+            
+            // bidimensional 2x2
+            float [ ,] matrix =new float[2, 2];
+            //float matrixFloat = float.Parse(matrix, CultureInfo.InvariantCulture);
+
+            
+
+            
+        }
+    }
+}
