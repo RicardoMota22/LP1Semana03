c961746ac53e26fa15672b44c137dcefd00dd68a
RicardoMota22 <ricardomota.artist@gmail.com>
2025-03-07 10:12:53 +0000

    Exercise 5, insert for

---
+++ Pow2Until/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Pow2Until
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            for ( int i = 1; i <= (1 << 5); i = i << 1)
+            {
+            Console . WriteLine ( i );
+            }
+        }
+    }
+}
