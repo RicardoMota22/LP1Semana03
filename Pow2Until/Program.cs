a3245c8269e84b0718388eaa6f7558092f15cc8d
RicardoMota22 <ricardomota.artist@gmail.com>
2025-03-07 10:24:42 +0000

    Exercise 5, call

--- Pow2Until/Program.cs
+++ Pow2Until/Program.cs
@@ -4,12 +4,20 @@ namespace Pow2Until
 {
     public class Program
     {
-        private static void Main(string[] args)
+        private static void PowersOf2Until5()
         {
             for ( int i = 1; i <= (1 << 5); i = i << 1)
             {
             Console . WriteLine ( i );
             }
         }
+        
+        static void Main()
+        {
+            PowersOf2Until5 ();
+            PowersOf2Until5 ();
+        }
+       
+        
     }
 }
