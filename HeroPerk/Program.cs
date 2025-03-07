f6bd311b8396b5bb5e73f7207ee040fac2e62ca4
RicardoMota22 <ricardomota.artist@gmail.com>
2025-03-07 00:08:39 +0000

    Exercise 4, work in progress, always unknown perk

--- HeroPerk/Program.cs
+++ HeroPerk/Program.cs
@@ -8,10 +8,11 @@ namespace HeroPerk
     [Flags]
     enum Perks
     {
-        WarpShift ,
-        Stealth,
-        AutoHeal ,
-        DoubleJump,
+        None = 0,
+        WarpShift = 1,
+        Stealth = 2,
+        AutoHeal = 4,
+        DoubleJump = 8
         
     };
     public class Program
@@ -24,20 +25,16 @@ namespace HeroPerk
         char[] letterToCount = { 'w', 'a', 's', 'd' };
         Perks p_Perks = Perks.None;
 
-        if (p_Perks = Perks.None)
-        {
-            Console.WriteLine();
-        }
 
-        if (letterToCount.Equals("w")== true)
+        if (letterToCount.Equals('w')== true)
         {
             p_Perks ^= Perks.WarpShift;
         }
-        if (letterToCount.Equals("a")== true)
+        if (letterToCount.Equals('a')== true)
         {
             p_Perks ^= Perks.Stealth;
         }
-        if (letterToCount.Equals("s")== true)
+        if (letterToCount.Equals('s')== true)
         {
             p_Perks ^= Perks.AutoHeal;
         }
@@ -45,14 +42,34 @@ namespace HeroPerk
         {
             p_Perks ^= Perks.DoubleJump;
         }
-        
-        
-            
+        else      //if it includes another letter
+        {
+            Console.WriteLine("!Unknown perk!");
+        }
        
 
-        if ((p_Perks &Perks.WarpShift) ==Perks.WarpShift)
-        Console.WriteLine("I heal you!");
-           
+        
+            
+        if (p_Perks == Perks.None)
+            {
+                Console.WriteLine("!No perks at all!");
+            }
+            else
+            {
+                Console.WriteLine(p_Perks);
+
+                if ((p_Perks & Perks.Stealth) != 0 && (p_Perks & Perks.DoubleJump) != 0)
+                {
+                    Console.WriteLine("!Silent jumper!");
+                }
+
+                if ((p_Perks & Perks.AutoHeal) == 0)
+                {
+                    Console.WriteLine("!Not gonna make it!");
+                }
+
+            }
+
         }
     }
 }
